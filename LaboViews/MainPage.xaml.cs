87f15538c4c65e2629174ee31204e1a23f72572f
MehmetCekicPXL <mehmet.cekic@student.pxl.be>
2025-10-07 15:40:40 +0200

    Animatie toegevoegd

--- LaboViews/MainPage.xaml.cs
+++ LaboViews/MainPage.xaml.cs
@@ -6,6 +6,22 @@
         {
             InitializeComponent();
         }
+
+        private  async void CardClicked(object sender, TappedEventArgs e)
+        {
+            Border? borderTapped = sender as Border;
+
+            if (borderTapped is null)
+                return;
+
+            if (borderTapped.BackgroundColor == Colors.Black)
+                borderTapped.BackgroundColor = Colors.Red;
+            else
+                borderTapped.BackgroundColor = Colors.Black;
+
+            await borderTapped.RotateYTo(360, 1500);
+            borderTapped.RotationY = 0;
+        }
     }
 
 }
